9b18d97cfa7eac07834689d72b735cc4291bb290
1661sons <julia.kerouac@gmail.com>
2018-09-10 02:42:54 -0400

    edited localhost in connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     mysql.createConnection({
-        host: "127.0.0.1",
+        host: "localhost",
         port: 3306,
         user: "root",
         password: "root",
