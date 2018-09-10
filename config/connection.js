5e01b2957299edadd300db9205df54112e3a1b21
1661sons <julia.kerouac@gmail.com>
2018-09-10 02:16:51 -0400

    testing connection

--- config/connection.js
+++ config/connection.js
@@ -8,11 +8,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     mysql.createConnection({
-        host: "nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "127.0.0.1",
         port: 3306,
-        user: "x2c6skqe6lvgv9c5",
-        password: "ttjz77m59co4nh6x",
-        database: "ava3ho5ldkyff1hl"
+        user: "root",
+        password: "root",
+        database: "stacks_db"
     });
 }
   
